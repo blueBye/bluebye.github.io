abec0a891c4a071ebcdf9c1f2fc64915495b9226
blueBye <navidshirmohammadi81@gmail.com>
2024-10-15 23:42:49 +0330

    added dropdown to toggle windows visibility

--- src/components/taskbar.tsx
+++ src/components/taskbar.tsx
@@ -3,7 +3,13 @@
 import React from 'react';
 import { windowStore$ } from "@/state/windowState";
 import { observer } from "@legendapp/state/react";
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {Button} from "@/components/ui/button";
 
 const Taskbar = observer(() => {
   const windows = windowStore$.windows.get();
@@ -12,9 +18,25 @@ const Taskbar = observer(() => {
     <div
       className="flex bg-gray-300 border-2 border-black shadow-md w-full h-10 z-10"
     >
-      {windows.map(w => {
-        return <div key={w.id}>{w.title}</div>
-      })}
+      <DropdownMenu>
+        <DropdownMenuTrigger>Programs</DropdownMenuTrigger>
+        <DropdownMenuContent className={"bg-gray-200 border-2 border-black rounded-none p-0 m-0 -gap-y-10"}>
+          {windows.map(w => {
+            return(
+              <DropdownMenuLabel key={w.id} className={"py-0"}>
+                <Button
+                  className={"w-full flex text-left justify-start m-0 py-0 px-2 h-8 hover:rounded-none"}
+                  variant="ghost"
+                  onClick={() => { windowStore$.toggleWindow(w.id) }}
+                >
+                  {w.title}
+                </Button>
+              </DropdownMenuLabel>
+            )
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
+
     </div>
   );
 });
